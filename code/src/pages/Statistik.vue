db4f9254d67b5c1c16a54fce9d41b4b94c8326d8
VKB User <lisa.brittinger@vkb.de>
2022-12-12 16:02:32 +0100

    Bilder hinzugefügt

--- code/src/pages/Statistik.vue
+++ code/src/pages/Statistik.vue
@@ -2,6 +2,40 @@
     <div>
       <h1>Statistik</h1>
     </div>
+
+    <q-page class="flex flex-center">
+    <q-card>
+        <img
+      alt="CoffeTracker Logo"
+      src="~/assets/piggy-money.webp"
+      style="width: 300px; height: 300px"
+    />
+
+
+    </q-card>
+    
+    <q-card>
+    <img
+      alt="CoffeTracker Logo"
+      src="~/assets/Messbecher.png"
+      style="width: 300px; height: 300px"
+    />
+
+    <q-card-section class="q-pt-none">
+        xcbkyfgkysfgklsdfksdf
+        sdfglnsdfg
+        sdflkgsldfjglsdfgjk
+      </q-card-section>
+</q-card>
+    
+<q-card>
+    <img
+      alt="CoffeTracker Logo"
+      src="~/assets/Messbecher.png"
+      style="width: 300px; height: 300px"
+    />
+</q-card>
+  </q-page>
   </template>
   
   <style></style>
